8e481d69a22882d29e8da59bff638d43d7399050
Ahmed Baig <ahmedsab9912@gmail.com>
2025-07-02 00:30:34 +0500

    Enhance NewExpense form with amount input, date picker, and category selection

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,6 +1,5 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-
+import 'package:expense_tracker/models/expense.dart';
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
 
@@ -9,11 +8,33 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  late Category _selectedCategory = Category.leisure;
   final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _amountController = TextEditingController();
+  DateTime? _selectedDate;
+
+  void _presentDatePicker()async {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    final selectedDate = await showDatePicker(context: context, firstDate: firstDate, lastDate: now);
+
+    setState(() {
+      _selectedDate = selectedDate;
+    });
+  }
+
+  void _submitExpenseData(){
+    final enteredAmount = double.tryParse(_amountController.text);
+    final amountIsInvalid = enteredAmount == null || enteredAmount <= 0;
+    if(_titleController.text.trim().isEmpty || amountIsInvalid || _selectedDate == null){
+      // Show Error
+    }
+  }
 
   @override
   void dispose() {
     _titleController.dispose();
+    _amountController.dispose();
     super.dispose();
   }
 
@@ -22,22 +43,66 @@ class _NewExpenseState extends State<NewExpense> {
     return Padding(
       padding: EdgeInsets.all(16),
       child: Column(
+        spacing: 2,
         children: [
           TextField(
             controller: _titleController,
             maxLength: 50,
+            keyboardType: TextInputType.text,
             decoration: InputDecoration(label: Text("Title")),
           ),
           Row(
             children: [
+              Expanded(
+                child: TextField(
+                  controller: _amountController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      prefixText: "\$",
+                      label: Text("Amount")),
+                ),
+              ),
+              SizedBox(width: 16,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                   Text(_selectedDate == null ? "No Date Selected" : formatter.format(_selectedDate!)),
+                  IconButton(
+                    onPressed: _presentDatePicker,
+                    icon: Icon(Icons.calendar_month),
+                  )
+                ],
+              )
+            ],
+          ),
+          Row(
+            children: [
+              SizedBox(height: 10,),
+              DropdownButton(
+                  value: _selectedCategory,
+                  items: Category.values.map((category)=> DropdownMenuItem(
+                value: category,
+                  child: Text(category.name.toString()))).toList(), onChanged: (value){
+                if(value == null){
+                  return;
+                }
+                setState(() {
+                  _selectedCategory = value;
+                });
+              }),
+              Spacer(),
               ElevatedButton(
                 onPressed: () {
-                  if (kDebugMode) {
-                    print(_titleController.text);
-                  }
+               Navigator.pop(context);
                 },
+                child: const Text("Cancel"),
+              ),
+              ElevatedButton(
+                onPressed: _submitExpenseData,
                 child: const Text("Save Expense"),
               ),
+
             ],
           ),
         ],
