a7e15b52e1f8437cc427205c12068ee9ecc9b87a
Ahmed Baig <ahmedsab9912@gmail.com>
2025-07-05 16:48:18 +0500

    Add ExpenseBucket class and enhance theme configuration in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,30 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Expenses(),
-  ));
-}
+var kColor = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
 
+var kDarkColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+);
+
+void main() {
+  runApp(
+    MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme
+      ),
+      theme: ThemeData().copyWith(
+        colorScheme: kColor,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColor.onPrimaryContainer,
+          foregroundColor: kColor.primaryContainer,
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Expenses(),
+      themeMode: ThemeMode.system,
+    ),
+  );
+}
--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
 import 'package:intl/intl.dart';
+
 const uuid = Uuid();
 
 DateFormat formatter = DateFormat.yMMMMd();
@@ -29,5 +30,23 @@ class Expense {
 
   String get formattedDate {
     return formatter.format(date);
+  }
+}
+
+class ExpenseBucket {
+  ExpenseBucket({required this.category, required this.expenses});
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+    : expenses = allExpenses
+          .where((expense) => expense.category == category)
+          .toList();
+  final Category category;
+  final List<Expense> expenses;
+  double get totalExpenses {
+    double sum = 0;
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+    return sum;
   }
 }
